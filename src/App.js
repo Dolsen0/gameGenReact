a368128e72ddfe7825617e85859867f48d781529
dolsen0 <derek.olsen@gmail.com>
2023-02-28 14:44:54 -0500

    rough adjustments to ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import GetPrompt from './components/getPrompt';
+import "./App.css";
+import GetPrompt from "./components/getPrompt";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <h1>Game Generator</h1>
-        <GetPrompt/>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header"></header>
+        <body>
+          <h1>Game Generator</h1>
+          <div className="gameBody" id="hardware">
+            <div id="screen">
+              <GetPrompt />
+            </div>
+            <Button variant="primary" id="Button">Generate</Button>
+          </div>
+        </body>
+      </div>
     </>
   );
 }
 
 export default App;
+
+// https://gameideasgenerator.web.app/
