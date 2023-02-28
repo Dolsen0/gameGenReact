c4974e8b02851f68733981f2ec0120ee151511cd
dolsen0 <derek.olsen@gmail.com>
2023-02-27 22:52:15 -0500

    communicating to server

---
+++ src/components/getPrompt.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+export default function GetPrompt() {
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://gamegenerator-cc1ec.web.app/main');
+        const jsonData = await response.text();
+        setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>{data}</p>
+      )}
+    </div>
+  );
+}
